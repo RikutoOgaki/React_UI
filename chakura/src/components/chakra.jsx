14b281967b0efdae4e1a7e95ca925aa50adc429b
Rikuto <orikutobasuke@gmail.com>
2023-01-27 12:26:03 +0900

    React chakra add

---
+++ chakura/src/components/chakra.jsx
@@ -0,0 +1,26 @@
+import { Box, Center, Flex } from '@chakra-ui/react'
+import React, { useState } from 'react'
+
+const chakra = () => {
+        const [btn,setBtn] = useState("100px");
+    const botton = ()=>{
+        console.log("blue");
+        setBtn("200px");
+    }
+
+  return (
+    <Box>
+        <Box w="100vw" h={"100px"} bgColor={"tomato"} display={"flex"} justifyContent={"center"} alignItems={"center"}>
+            <h1>こんにちは</h1>
+        </Box>
+        <Flex w={"100vw"} h={"100px"} bgColor={"aqua"} justifyContent={"center"} alignItems={"center"}>
+            <h1>こんにちは</h1>
+        </Flex>
+        <Center as='button' w={btn} h={"100px"} bgColor={"blue"} boxShadow={"1px 2px 5px #000"}
+        onClick={botton}>
+        </Center>
+    </Box>
+  )
+}
+
+export default chakra
