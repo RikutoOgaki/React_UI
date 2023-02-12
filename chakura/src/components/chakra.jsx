2a8f47447eb4d4ca8f47d7d24ed8e98df37e5cac
Rikuto <orikutobasuke@gmail.com>
2023-02-12 23:19:06 +0900

    React_UI add

--- chakura/src/components/chakra.jsx
+++ chakura/src/components/chakra.jsx
@@ -16,8 +16,9 @@ const chakra = () => {
         <Flex w={"100vw"} h={"100px"} bgColor={"aqua"} justifyContent={"center"} alignItems={"center"}>
             <h1>こんにちは</h1>
         </Flex>
-        <Center as='button' w={btn} h={"100px"} bgColor={"blue"} boxShadow={"1px 2px 5px #000"}
+        <Center as='button' w={btn} h={"100px"} bgColor={"blue"} boxShadow={"1px 2px 5px #000"} color={"#fff"}
         onClick={botton}>
+            <h1>こんにちは</h1>
         </Center>
     </Box>
   )
